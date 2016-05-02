b5749d361ae63d3bca0158a9df3c95f8715d1266
Marcel Klehr <mklehr@gmx.net>
2016-05-02 14:52:11 +0200

    Switch to primus

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-var shoe = require('shoe2')
+var Primus = require('primus')
   , dataplex = require('dataplex')
   , co = require('co')
   , gulf = require('gulf')
@@ -31,103 +31,125 @@ function setup(plugin, imports, register) {
     , broadcast = imports.broadcast
 
   hooks.on('http:listening', function*(server) {
-    sock.install(server, '/socket')
-  })
+    var primus = new Primus(server, {
+      pathname:'/stream'
+    , authorization: null
+    , parser: 'binary'
+    })
 
-  var sock = shoe(function(stream) {
-    var plex = dataplex()
-    stream.pipe(plex).pipe(stream)
+    primus.use('client-side-binary-converter', {
+      client: function(primus) {
+        primus.transform('incoming', function (packet) {
+          packet.data = new Buffer(packet.data)
+        });
 
-    plex.add('/document/:id/sync', function(opts) {
-      // Set up link authorization
-      var link = new gulf.Link({
-        authenticate: function(credentials, cb) {
-          co(function*() {
-            return yield auth.authenticate('token', credentials)
-          })
-          .then((user) => {
-            cb(null, user.id)
-          })
-          .catch(cb)
-        }
-      , authorizeWrite: function(msg, user, cb) {
-          co(function*() {
-            if(!plex.user) return false
-            var allowed = false
-            switch(msg[0]) {
-              case 'edit':
-                allowed = yield auth.authorize(plex.user, 'document:change', {document: opts.id})
-                break;
-              case 'ack':
-              case 'requestInit':
-                allowed = yield auth.authorize(plex.user, 'document:read', {document: opts.id})
-                break;
-            }
-            return allowed
-          })
-          .then(function(allowed) {
-            cb(null, allowed)
-          })
-          .catch(cb)
-        }
-      , authorizeRead:function(msg, user, cb) {
-          co(function*() {
-            if(!plex.user) return false
-            var allowed = false
-            switch(msg[0]) {
-              case 'edit':
-                allowed = yield auth.authorize(plex.user, 'document:read', {document: opts.id})
-                break;
-              case 'ack':
-              case 'init':
-                allowed = yield auth.authorize(plex.user, 'document:read', {document: opts.id})
-                break;
-            }
-            return allowed
-          })
-          .then(function(allowed) {
-            cb(null, allowed)
-          })
-          .catch(cb)
-        }
+        primus.transform('outgoing', function (packet) {
+          // convert node Buffers to Arraybuffers as per
+          // https://stackoverflow.com/questions/8609289/convert-a-binary-nodejs-buffer-to-javascript-arraybuffer#12101012
+          // var arrayBuffer = new Uint8Array(nodeBuffer).buffer
+          packet.data = (new Uint8Array(packet.data)).buffer
+        });
+      }
+    })
+
+    primus.on('connection',function(stream) {
+      var plex = dataplex()
+      stream.pipe(plex).pipe(stream)
+
+      plex.add('/document/:id/sync', function(opts) {
+	// Set up link authorization
+	var link = new gulf.Link({
+	  authenticate: function(credentials, cb) {
+	    co(function*() {
+	      return yield auth.authenticate('token', credentials)
+	    })
+	    .then((user) => {
+	      cb(null, user.id)
+	    })
+	    .catch(cb)
+	  }
+	, authorizeWrite: function(msg, user, cb) {
+	    co(function*() {
+	      if(!plex.user) return false
+	      var allowed = false
+	      switch(msg[0]) {
+		case 'edit':
+		  allowed = yield auth.authorize(plex.user, 'document:change', {document: opts.id})
+		  break;
+		case 'ack':
+		case 'requestInit':
+		  allowed = yield auth.authorize(plex.user, 'document:read', {document: opts.id})
+		  break;
+	      }
+	      return allowed
+	    })
+	    .then(function(allowed) {
+	      cb(null, allowed)
+	    })
+	    .catch(cb)
+	  }
+	, authorizeRead:function(msg, user, cb) {
+	    co(function*() {
+	      if(!plex.user) return false
+	      var allowed = false
+	      switch(msg[0]) {
+		case 'edit':
+		  allowed = yield auth.authorize(plex.user, 'document:read', {document: opts.id})
+		  break;
+		case 'ack':
+		case 'init':
+		  allowed = yield auth.authorize(plex.user, 'document:read', {document: opts.id})
+		  break;
+	      }
+	      return allowed
+	    })
+	    .then(function(allowed) {
+	      cb(null, allowed)
+	    })
+	    .catch(cb)
+	  }
+	})
+
+	co(function*() {
+	  // load document and add slave link
+	  var doc = yield sync.getDocument(opts.id)
+	  doc.attachSlaveLink(link)
+	})
+	.then(function() {}, function(er) { console.log(er.stack || er)})
+
+	return link
       })
+
+      plex.add('/document/:id/broadcast', function(opts) {
+	var b = broadcast.document(opts.id, plex.user)
+	stream.on('close', function() {
+	  b.emit('close')
+	})
+	return b
+      })
+
+      plex.add('/authenticate', function() {
+	return through(function(chunk, enc, cb) {
+	  var that = this
+	  co(function*() {
+	    try {
+	      plex.user = yield auth.authenticate('token', chunk.toString('utf8'))
+	      if(plex.user) return that.push(JSON.stringify({authenticated: true}))
+	    }catch(e) { }
+	    that.push(JSON.stringify({authenticated: false }))
+	  })
+	  .then(cb)
+	  .catch(cb)
+	})
+      })
+
 
       co(function*() {
-        // load document and add slave link
-        var doc = yield sync.getDocument(opts.id)
-        doc.attachSlaveLink(link)
-      })
-      .then(function() {}, function(er) { console.log(er.stack || er)})
-
-      return link
+	yield hooks.callHook('interface-stream:connect', plex)
+      }).then(function() {})
     })
-
-    plex.add('/document/:id/broadcast', function(opts) {
-      var b = broadcast.document(opts.id, plex.user)
-      stream.on('close', function() {
-        b.emit('close')
-      })
-      return b
-    })
-
-    plex.add('/authenticate', function() {
-      return through(function(chunk, enc, cb) {
-        var that = this
-        co(function*() {
-          try {
-            plex.user = yield auth.authenticate('token', chunk.toString('utf8'))
-            if(plex.user) return that.push(JSON.stringify({authenticated: true}))
-          }catch(e) { }
-          that.push(JSON.stringify({authenticated: false }))
-        })
-        .then(cb)
-        .catch(cb)
-      })
-    })
-
-
     co(function*() {
-      yield hooks.callHook('shoe-interface:connect', plex)
+      yield hooks.callHook('interface-stream:setup', primus)
     }).then(function() {})
   })
 
